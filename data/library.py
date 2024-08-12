a52a258cc17fe0ff11f4d131af9e6f18d05a68ed
cafalchio <mcafalchio@gmail.com>
2024-08-12 22:01:16 +0100

    fix folder

--- data/library.py
+++ data/library.py
@@ -2,7 +2,7 @@ import json
 from glob import glob
 import os
 
-SOURCE_FOLDER = os.path.expanduser("/portainer/downloads/lidarr/**/")  # Expand the tilde to full path
+SOURCE_FOLDER = os.path.expanduser("~/portainer/downloads/lidarr/**/")  # Expand the tilde to full path
 MUSIC_FILE = 'musics.json'
 
 def get_local_library():
