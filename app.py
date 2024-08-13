1ed48bea5611fa308a25dc6f508f0b5e14cefaed
cafalchio <mcafalchio@gmail.com>
2024-08-13 10:43:53 +0100

    Started the project

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import urllib
 
 from flask import Flask, render_template, jsonify
 from data.library import get_local_library
@@ -16,20 +15,10 @@ def library():
     files = get_local_library()
     return render_template("music.html", files=files)
 
-@app.route("/api/play/<path:song_path>")
-def play(song_path):
-    song_path = urllib.parse.unquote(song_path)
-    if os.path.isfile(song_path):
-        try:
-            # Using subprocess to play the song
-            command = ["mpg123", "-a", "hw:CARD=rockchipes8388,DEV=0", song_path]
-            subprocess.Popen(command)
-            print(f"Playing {song_path}")
-            return jsonify({"message": f"Playing {song_path}"}), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    else:
-        return jsonify({"error": "Song not found."}), 404
+@app.route("/api/play/<string:id>")
+def play(id):
+    print(f"Playing {id}")
+    pass
 
 
 if __name__ == '__main__':
--- data/library.py
+++ data/library.py
@@ -1,33 +1,9 @@
-import json
-import urllib
-from glob import glob
 import os
 
 SOURCE_FOLDER = os.path.expanduser("~/portainer/downloads/lidarr")  # Expand the tilde to full path
 MUSIC_FILE = './musics.json'
 
 def get_local_library():
-    if os.path.isfile(MUSIC_FILE):
-        try:
-            with open(MUSIC_FILE, "r", encoding='utf-8') as f:
-                files = json.load(f)
-                # Check if the loaded files are indeed a list
-                if isinstance(files, list):
-                    return files
-                else:
-                    print("JSON file is not a list, recreating it.")
-        except (json.JSONDecodeError, ValueError) as e:
-            print(f"Error reading JSON file: {e}, recreating the file.")
+   """This function return the local library"""
+   return []
 
-    print("Looking for files")
-    files = glob(os.path.join(SOURCE_FOLDER, '**', '*.mp3'), recursive=True)
-    files = [urllib.parse.quote(file) for file in files]
-
-    # Check if any files were found before writing to the JSON file
-    if files:
-        with open(MUSIC_FILE, 'w', encoding='utf-8') as f:
-            print("Writing files to music.json")
-            json.dump(files, f, ensure_ascii=False, indent=4)
-    else:
-        print("No MP3 files found.")
-    return files
